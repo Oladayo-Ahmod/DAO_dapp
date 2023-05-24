541ff5467ae3c8196b5690f6603743fa11daab47
oladayo ahmod <oladayoahmod112@gmail.com>
2023-05-24 14:20:14 -0700

    create proposal

--- contract/contracts/DAO.sol
+++ contract/contracts/DAO.sol
@@ -21,17 +21,39 @@ contract Dao {
         uint256 amount;
         uint256 upVote;
         uint256 downVotes;
+        uint256 duration;
         string title;
         string description;
         bool paid;
         bool passed;
         address payable beneficiary;
-
+        address propoper;
+        address executor;
     }
 
     struct Voted {
         address voter;
         uint256 timestamp;
         bool chosen;
+    }
+
+    modifier stakeholderOnly(string memory message) {
+        require(hasRole(STAKEHOLDER_ROLE,msg.sender),message)
+    }
+
+    function createProposal (
+        string calldata title,
+        string calldata description,
+        address beneficiary,
+        uint256 amount
+    )external stakeholderOnly("Only stakeholders are allowed to create Proposals") returns(Proposals memory){
+        uint256 currentID = totalProposals++;
+        Proposals storage StakeholderProposal = raisedProposals
+        StakeholderProposal.id = currentID;
+        StakeholderProposal.amount = amount;
+        StakeholderProposal.title = title;
+        StakeholderProposal.description = description;
+        StakeholderProposal.beneficiary = payable(beneficiary);
+        StakeholderProposal.duration = block.timestamp + MIN_VOTE_DURATION;
     }
 }
