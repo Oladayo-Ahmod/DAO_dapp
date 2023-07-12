eed2be29783e3ff883771812833c97f5657a98a1
oladayo ahmod <oladayoahmod112@gmail.com>
2023-07-11 20:11:16 -0700

    modify next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    images : {
-      unoptimized : true
-    }
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+    };
+  },
+  images: {
+    unoptimized: true,
+  },
+
 }
 
 module.exports = nextConfig
